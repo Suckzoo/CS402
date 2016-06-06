0c04a989024c5976eeea7d59c4c82c135912ca15
suckzoo <tjkj555@gmail.com>
2016-06-07 07:15:57 +0900

    Finalized cw3

--- HW3/verifier/verifier.py
+++ HW3/verifier/verifier.py
@@ -12,8 +12,8 @@ class Node(object):
         self.l_number = l_number
         self.edge = []
 
-    def push_edge(self, condition, next_line):
-        self.edge.append((condition, next_line))
+    def push_edge(self, condition, next_line, negation = False):
+        self.edge.append((condition, next_line, negation))
     
 class Pattern:
     ptn_declare = re.compile('(int .*;|byte .*;)')
@@ -22,7 +22,7 @@ class Pattern:
     ptn_throw = re.compile('throw .*')
     ptn_testme = re.compile('public void testMe\(int(, int)*\)')
     ptn_goto = re.compile('^goto .*')
-    ptn_expression = re.compile('[\$A-Za-z0-9]+ = .*')
+    ptn_expression = re.compile('[\$A-Za-z_][\$A-Za-z0-9_]+ = .*')
     ptn_param = re.compile('@parameter[0-9]+: int;')
 
 class ShimpleInstance(object):
@@ -56,24 +56,41 @@ class ShimpleInstance(object):
     def declare_token(self, x):
         self.variables[x] = Int(x)
 
-    def add_expression(self, expr, negation = False):
+    def add_expression(self, expr, phi = None, negation = False):
+        if 'Phi' in expr:
+            phi_dict = {}
+            phi_index = expr.find('Phi(')
+            phi_values = expr.split('Phi(')[1][:-1].split(', ')
+            for values in phi_values:
+                sp_values = values.split(' #')
+                if sp_values[1] == str(phi):
+                    expr = expr[:phi_index] + sp_values[0]
+                    break
         def __wrap_variables(match):
             return 'self.variables[\'%s\']' % match.group(0)
-        sub_expr = re.sub(r'[\$A-Za-z][\$A-Za-z0-9]+', __wrap_variables, expr)
+        sub_expr = re.sub(r'[\$A-Za-z_][\$A-Za-z0-9_]+', __wrap_variables, expr)
         if negation:
             sub_expr = 'Not(' + sub_expr + ')'
         sub_expr = 'self.solver.add(' + sub_expr + ')'
         try:
             eval(sub_expr)
         except:
-            return False
+            # print 'Error: ' + sub_expr
+            return False | negation
         return True
 
     def interpret(self):
         self.solver = Solver()
-        def __dfs__(node):
+        def __dfs__(node, phi = None):
             line = self.code[node.l_number]
-            print '(%d) %s' % (node.l_number, line)
+            next_phi = None
+            match_phi = re.match(r'\([0-9]+\)', line)
+            if match_phi:
+                raw_phi = match_phi.group(0)
+                next_phi = int(raw_phi[1:-1])
+                line = line[len(raw_phi):].strip()
+                # print 'next phi is ' + str(next_phi)
+            # print '(%d) %s' % (node.l_number + 1, line)
             if Pattern.ptn_declare.search(line):
                 tokens = None
                 if line[:3] == 'int':
@@ -85,22 +102,25 @@ class ShimpleInstance(object):
             elif Pattern.ptn_param.search(line):
                 self.declare_param(line.split(':=')[0].strip())
             elif Pattern.ptn_expression.search(line):
-                self.add_expression(line.replace('=', '==')[:-1])
+                self.add_expression(line.replace('=', '==')[:-1], phi)
             elif Pattern.ptn_throw.search(line):
                 if self.solver.check() == sat:
                     model = self.solver.model()
                     t_model = []
                     for param in self.params:
                         t_model.append(model.evaluate(self.variables[param]))
+                    # print model
                     raise InvalidException(t_model)
+            elif phi and not next_phi:
+                next_phi = phi
             for edge in node.edge:
                 if edge[0]:
                     self.solver.push()
-                    if self.add_expression(edge[0]):
-                        __dfs__(self.flow_graph[edge[1]])
+                    if self.add_expression(edge[0], phi, edge[2]):
+                        __dfs__(self.flow_graph[edge[1]], next_phi)
                     self.solver.pop()
-                    self.add_expression(edge[0], True)
-                __dfs__(self.flow_graph[edge[1]])
+                else:
+                    __dfs__(self.flow_graph[edge[1]], next_phi)
 
         __dfs__(self.root)
 
@@ -129,17 +149,22 @@ class ShimpleInstance(object):
                 else:
                     current_node = Node(number)
                 self.flow_graph[number] = current_node
+                match_phi = re.match(r'\([0-9]+\)', line)
+                if match_phi:
+                    raw_phi = match_phi.group(0)
+                    line = line[len(raw_phi):].strip()
                 if line == '}':
                     break
                 elif Pattern.ptn_branch.search(line):
                     condition = line.split('if')[1].split('goto')[0].strip()
                     l_number = self.labels[line.split('goto')[1][:-1].strip()]
                     current_node.push_edge(condition, l_number + 1)
+                    current_node.push_edge(condition, number + 1, True)
                 elif Pattern.ptn_goto.search(line):
                     l_number = self.labels[line[5:-1]]
                     current_node.push_edge(None, l_number + 1)
-                    continue
-                current_node.push_edge(None, number + 1)
+                else:
+                    current_node.push_edge(None, number + 1)
 
     def scan(self):
         shimple_file = open(self.file_name + '.shimple')
